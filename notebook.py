8a4e12a7d08a29062f8a12498099c6cee34260cf
Bahadır ÖZCANLI <100536921+Bahadir-OZCANLI@users.noreply.github.com>
2023-06-08 21:10:42 +0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ## 1. Google Play Store apps and reviews
-# <p>Mobile apps are everywhere. They are easy to create and can be lucrative. Because of these two factors, more and more apps are being developed. In this notebook, we will do a comprehensive analysis of the Android app market by comparing over ten thousand apps in Google Play across different categories. We'll look for insights in the data to devise strategies to drive growth and retention.</p>
-# <p><img src="https://assets.datacamp.com/production/project_619/img/google_play_store.png" alt="Google Play logo"></p>
-# <p>Let's take a look at the data, which consists of two files:</p>
-# <ul>
-# <li><code>apps.csv</code>: contains all the details of the applications on Google Play. There are 13 features that describe a given app.</li>
-# <li><code>user_reviews.csv</code>: contains 100 reviews for each app, <a href="https://www.androidpolice.com/2019/01/21/google-play-stores-redesigned-ratings-and-reviews-section-lets-you-easily-filter-by-star-rating/">most helpful first</a>. The text in each review has been pre-processed and attributed with three new features: Sentiment (Positive, Negative or Neutral), Sentiment Polarity and Sentiment Subjectivity.</li>
-# </ul>
+#  Google Play Store apps and reviews
+# Mobile apps are everywhere. They are easy to create and can be lucrative. Because of these two factors, more and more apps are being developed. In this notebook, we will do a comprehensive analysis of the Android app market by comparing over ten thousand apps in Google Play across different categories. We'll look for insights in the data to devise strategies to drive growth and retention.
+# Let's take a look at the data, which consists of two files:
 
 # In[22]:
 
@@ -27,11 +19,7 @@ print('Total number of apps in the dataset = ', apps.sum())
 print(apps.head())
 
 
-# ## 2. Data cleaning
-# <p>Data cleaning is one of the most essential subtask any data science project. Although it can be a very tedious process, it's worth should never be undermined.</p>
-# <p>By looking at a random sample of the dataset rows (from the above task), we observe that some entries in the columns like <code>Installs</code> and <code>Price</code> have a few special characters (<code>+</code> <code>,</code> <code>$</code>) due to the way the numbers have been represented. This prevents the columns from being purely numeric, making it difficult to use them in subsequent future mathematical calculations. Ideally, as their names suggest, we would want these columns to contain only digits from [0-9].</p>
-# <p>Hence, we now proceed to clean our data. Specifically, the special characters <code>,</code> and <code>+</code> present in <code>Installs</code> column and <code>$</code> present in <code>Price</code> column need to be removed.</p>
-# <p>It is also always a good practice to print a summary of your dataframe after completing data cleaning. We will use the <code>info()</code> method to acheive this.</p>
+#  2. Data cleaning
 
 # In[24]:
 
@@ -52,9 +40,7 @@ for col in cols_to_clean:
 print(apps.info())
 
 
-# ## 3. Correcting data types
-# <p>From the previous task we noticed that <code>Installs</code> and <code>Price</code> were categorized as <code>object</code> data type (and not <code>int</code> or <code>float</code>) as we would like. This is because these two columns originally had mixed input types: digits and special characters. To know more about Pandas data types, read <a href="https://datacarpentry.org/python-ecology-lesson/04-data-types-and-format/">this</a>.</p>
-# <p>The four features that we will be working with most frequently henceforth are <code>Installs</code>, <code>Size</code>, <code>Rating</code> and <code>Price</code>. While <code>Size</code> and <code>Rating</code> are both <code>float</code> (i.e. purely numerical data types), we still need to work on <code>Installs</code> and <code>Price</code> to make them numeric.</p>
+#  3. Correcting data types
 
 # In[26]:
 
@@ -71,18 +57,9 @@ apps['Price'] = apps['Price'].astype(float)
 print(apps.info())
 
 
-# ## 4. Exploring app categories
-# <p>With more than 1 billion active users in 190 countries around the world, Google Play continues to be an important distribution platform to build a global audience. For businesses to get their apps in front of users, it's important to make them more quickly and easily discoverable on Google Play. To improve the overall search experience, Google has introduced the concept of grouping apps into categories.</p>
-# <p>This brings us to the following questions:</p>
-# <ul>
-# <li>Which category has the highest share of (active) apps in the market? </li>
-# <li>Is any specific category dominating the market?</li>
-# <li>Which categories have the fewest number of apps?</li>
-# </ul>
-# <p>We will see that there are <code>33</code> unique app categories present in our dataset. <em>Family</em> and <em>Game</em> apps have the highest market prevalence. Interestingly, <em>Tools</em>, <em>Business</em> and <em>Medical</em> apps are also at the top.</p>
+# 4. Exploring app categories
 
 # In[28]:
-
 
 import plotly
 plotly.offline.init_notebook_mode(connected=True)
@@ -106,9 +83,7 @@ data = [go.Bar(
 plotly.offline.iplot(data)
 
 
-# ## 5. Distribution of app ratings
-# <p>After having witnessed the market share for each category of apps, let's see how all these apps perform on an average. App ratings (on a scale of 1 to 5) impact the discoverability, conversion of apps as well as the company's overall brand image. Ratings are a key performance indicator of an app.</p>
-# <p>From our research, we found that the average volume of ratings across all app categories is <code>4.17</code>. The histogram plot is skewed to the left indicating that the majority of the apps are highly rated with only a few exceptions in the low-rated apps.</p>
+# 5. Distribution of app ratings
 
 # In[30]:
 
@@ -136,19 +111,9 @@ layout = {'shapes': [{
 plotly.offline.iplot({'data': data, 'layout': layout})
 
 
-# ## 6. Size and price of an app
-# <p>Let's now examine app size and app price. For size, if the mobile app is too large, it may be difficult and/or expensive for users to download. Lengthy download times could turn users off before they even experience your mobile app. Plus, each user's device has a finite amount of disk space. For price, some users expect their apps to be free or inexpensive. These problems compound if the developing world is part of your target market; especially due to internet speeds, earning power and exchange rates.</p>
-# <p>How can we effectively come up with strategies to size and price our app?</p>
-# <ul>
-# <li>Does the size of an app affect its rating? </li>
-# <li>Do users really care about system-heavy apps or do they prefer light-weighted apps? </li>
-# <li>Does the price of an app affect its rating? </li>
-# <li>Do users always prefer free apps over paid apps?</li>
-# </ul>
-# <p>We find that the majority of top rated apps (rating over 4) range from 2 MB to 20 MB. We also find that the vast majority of apps price themselves under \$10.</p>
+# 6. Size and price of an app
 
 # In[32]:
-
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 import seaborn as sns
@@ -172,13 +137,9 @@ paid_apps = apps_with_size_and_rating_present[apps_with_size_and_rating_present[
 plt2 = sns.jointplot(x = paid_apps['Price'], y = paid_apps['Rating'])
 
 
-# ## 7. Relation between app category and app price
-# <p>So now comes the hard part. How are companies and developers supposed to make ends meet? What monetization strategies can companies use to maximize profit? The costs of apps are largely based on features, complexity, and platform.</p>
-# <p>There are many factors to consider when selecting the right pricing strategy for your mobile app. It is important to consider the willingness of your customer to pay for your app. A wrong price could break the deal before the download even happens. Potential customers could be turned off by what they perceive to be a shocking cost, or they might delete an app they’ve downloaded after receiving too many ads or simply not getting their money's worth.</p>
-# <p>Different categories demand different price ranges. Some apps that are simple and used daily, like the calculator app, should probably be kept free. However, it would make sense to charge for a highly-specialized medical app that diagnoses diabetic patients. Below, we see that <em>Medical and Family</em> apps are the most expensive. Some medical apps extend even up to \$80! All game apps are reasonably priced below \$20.</p>
+# 7. Relation between app category and app price
 
 # In[34]:
-
 
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
@@ -198,12 +159,9 @@ apps_above_200 = apps[apps['Price'] > 200]
 apps_above_200[['Category', 'App', 'Price']]
 
 
-# ## 8. Filter out "junk" apps
-# <p>It looks like a bunch of the really expensive apps are "junk" apps. That is, apps that don't really have a purpose. Some app developer may create an app called <em>I Am Rich Premium</em> or <em>most expensive app (H)</em> just for a joke or to test their app development skills. Some developers even do this with malicious intent and try to make money by hoping people accidentally click purchase on their app in the store.</p>
-# <p>Let's filter out these junk apps and re-do our visualization.</p>
+# 8. Filter out "junk" apps
 
 # In[36]:
-
 
 # Select apps priced below $100
 apps_under_100 = popular_app_cats[popular_app_cats['Price'] < 100]
@@ -216,20 +174,7 @@ ax = sns.stripplot(x = 'Price', y = 'Category', data = popular_app_cats, jitter 
 ax.set_title('App pricing trend across categories after filtering for junk apps')
 
 
-# ## 9. Popularity of paid apps vs free apps
-# <p>For apps in the Play Store today, there are five types of pricing strategies: free, freemium, paid, paymium, and subscription. Let's focus on free and paid apps only. Some characteristics of free apps are:</p>
-# <ul>
-# <li>Free to download.</li>
-# <li>Main source of income often comes from advertisements.</li>
-# <li>Often created by companies that have other products and the app serves as an extension of those products.</li>
-# <li>Can serve as a tool for customer retention, communication, and customer service.</li>
-# </ul>
-# <p>Some characteristics of paid apps are:</p>
-# <ul>
-# <li>Users are asked to pay once for the app to download and use it.</li>
-# <li>The user can't really get a feel for the app before buying it.</li>
-# </ul>
-# <p>Are paid apps installed as much as free apps? It turns out that paid apps have a relatively lower number of installs than free apps, though the difference is not as stark as I would have expected!</p>
+# 9. Popularity of paid apps vs free apps
 
 # In[38]:
 
@@ -258,13 +203,9 @@ data = [trace0, trace1]
 plotly.offline.iplot({'data': data, 'layout': layout})
 
 
-# ## 10. Sentiment analysis of user reviews
-# <p>Mining user review data to determine how people feel about your product, brand, or service can be done using a technique called sentiment analysis. User reviews for apps can be analyzed to identify if the mood is positive, negative or neutral about that app. For example, positive words in an app review might include words such as 'amazing', 'friendly', 'good', 'great', and 'love'. Negative words might be words like 'malware', 'hate', 'problem', 'refund', and 'incompetent'.</p>
-# <p>By plotting sentiment polarity scores of user reviews for paid and free apps, we observe that free apps receive a lot of harsh comments, as indicated by the outliers on the negative y-axis. Reviews for paid apps appear never to be extremely negative. This may indicate something about app quality, i.e., paid apps being of higher quality than free apps on average. The median polarity score for paid apps is a little higher than free apps, thereby syncing with our previous observation.</p>
-# <p>In this notebook, we analyzed over ten thousand apps from the Google Play Store. We can use our findings to inform our decisions should we ever wish to create an app ourselves.</p>
+# 10. Sentiment analysis of user reviews
 
 # In[40]:
-
 
 # Load user_reviews.csv
 reviews_df = pd.read_csv('datasets/user_reviews.csv')
